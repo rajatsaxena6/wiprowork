ef039e899bb9aef445b66e7e9d5a085ef40e5817
rajatsaxena6 <rajat.saxena6@wipro.com>
2016-07-26 13:15:39 +0530

    bootstrap

---
+++ assignment2/mybootstrapwebsite/includes/js/script.js
@@ -0,0 +1,22 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+
+
+$(function() {
+	
+	$('#bookme').click(function(e) {
+		
+		e.preventDefault();
+		
+		$('#successalert').slideDown();
+		
+	});
+});
